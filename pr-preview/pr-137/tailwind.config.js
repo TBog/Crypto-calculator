575bd8d7b86701eebbe048cef1e573b4f7ddb822
TBog <TBog@users.noreply.github.com>
2026-01-05 13:14:36 +0000

    Deploy preview for PR 137 🛫

---
+++ pr-preview/pr-137/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    "./dist/index.html",
+    "./dist/script.js",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
